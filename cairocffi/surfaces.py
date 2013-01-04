d8ff53198ed3a1117e2bd16e039b41519eda9dd2
Simon Sapin <simon.sapin@exyr.org>
2013-01-05 00:12:49 +0100

    Document all Surface methods
    … but not methods of Surface subclasses, yet.

--- cairocffi/surfaces.py
+++ cairocffi/surfaces.py
@@ -10,6 +10,7 @@
 
 """
 
+import io
 import sys
 import ctypes
 
@@ -184,10 +185,6 @@ class Surface(object):
         return Surface._from_pointer(cairo.cairo_surface_create_for_rectangle(
             self._pointer, x, y, width, height))
 
-    def copy_page(self):
-        cairo.cairo_surface_copy_page(self._pointer)
-        self._check_status()
-
     def get_content(self):
         """
         :returns: the :ref:`CONTENT` of this surface \
@@ -215,44 +212,155 @@ class Surface(object):
         """
         return bool(cairo.cairo_surface_has_show_text_glyphs(self._pointer))
 
+    def set_device_offset(self, x_offset, y_offset):
+        """ Sets an offset that is added to the device coordinates
+        determined by the CTM when drawing to surface.
+        One use case for this method is
+        when we want to create a :class:`Surface` that redirects drawing
+        for a portion of an onscreen surface
+        to an offscreen surface in a way that is
+        completely invisible to the user of the cairo API.
+        Setting a transformation via :meth:`Context.translate`
+        isn't sufficient to do this,
+        since methods like :meth:`Context.device_to_user`
+        will expose the hidden offset.
+
+        Note that the offset affects drawing to the surface
+        as well as using the surface in a source pattern.
+
+        :param x_offset:
+            The offset in the X direction, in device units
+        :param y_offset:
+            The offset in the Y direction, in device units
+
+        """
+        cairo.cairo_surface_set_device_offset(
+            self._pointer, x_offset, y_offset)
+        self._check_status()
+
     def get_device_offset(self):
+        """Returns the previous device offset set by :meth:`set_device_offset`.
+
+        :returns: ``(x_offset, y_offset)``
+
+        """
         offsets = ffi.new('double[2]')
         cairo.cairo_surface_get_device_offset(
             self._pointer, offsets + 0, offsets + 1)
         return tuple(offsets)
 
-    def set_device_offset(self, x_offset, y_offset):
-        cairo.cairo_surface_set_device_offset(
-            self._pointer, x_offset, y_offset)
+    def set_fallback_resolution(self, x_pixels_per_inch, y_pixels_per_inch):
+        """
+        Set the horizontal and vertical resolution for image fallbacks.
+
+        When certain operations aren't supported natively by a backend,
+        cairo will fallback by rendering operations to an image
+        and then overlaying that image onto the output.
+        For backends that are natively vector-oriented,
+        this function can be used to set the resolution
+        used for these image fallbacks,
+        (larger values will result in more detailed images,
+        but also larger file sizes).
+
+        Some examples of natively vector-oriented backends are
+        the ps, pdf, and svg backends.
+
+        For backends that are natively raster-oriented,
+        image fallbacks are still possible,
+        but they are always performed at the native device resolution.
+        So this function has no effect on those backends.
+
+        .. note::
+
+            The fallback resolution only takes effect
+            at the time of completing a page
+            (with :meth:`show_page` or :meth:`copy_page`)
+            so there is currently no way to have
+            more than one fallback resolution in effect on a single page.
+
+        The default fallback resoultion is
+        300 pixels per inch in both dimensions.
+
+        :param x_pixels_per_inch: horizontal resolution in pixels per inch
+        :type x_pixels_per_inch: float
+        :param y_pixels_per_inch: vertical resolution in pixels per inch
+        :type y_pixels_per_inch: float
+
+        """
+        cairo.cairo_surface_set_fallback_resolution(
+            self._pointer, x_pixels_per_inch, y_pixels_per_inch)
         self._check_status()
 
     def get_fallback_resolution(self):
+        """Returns the previous fallback resolution
+        set by :meth:`set_fallback_resolution`,
+        or default fallback resolution if never set.
+
+        :returns: ``(x_pixels_per_inch, y_pixels_per_inch)``
+
+        """
         ppi = ffi.new('double[2]')
         cairo.cairo_surface_get_fallback_resolution(
             self._pointer, ppi + 0, ppi + 1)
         return tuple(ppi)
 
-    def set_fallback_resolution(self, x_pixels_per_inch, y_pixels_per_inch):
-        cairo.cairo_surface_set_fallback_resolution(
-            self._pointer, x_pixels_per_inch, y_pixels_per_inch)
-        self._check_status()
-
     def get_font_options(self):
+        """ Retrieves the default font rendering options for the surface.
+
+        This allows display surfaces to report the correct subpixel order
+        for rendering on them,
+        print surfaces to disable hinting of metrics and so forth.
+        The result can then be used with :class:`ScaledFont`.
+
+        :returns: A new :class:`FontOptions` object.
+
+        """
         font_options = FontOptions()
         cairo.cairo_surface_get_font_options(
             self._pointer, font_options._pointer)
         return font_options
 
-    def get_mime_data(self, mime_type):
-        buffer_address = ffi.new('unsigned char **')
-        buffer_length = ffi.new('unsigned long *')
-        mime_type = ffi.new('char[]', mime_type.encode('utf8'))
-        cairo.cairo_surface_get_mime_data(
-            self._pointer, mime_type, buffer_address, buffer_length)
-        return (ffi.buffer(buffer_address[0], buffer_length[0])
-                if buffer_address[0] != ffi.NULL else None)
-
     def set_mime_data(self, mime_type, data):
+        """
+         Attach an image in the format :obj:`mime_type` to this surface.
+
+         To remove the data from a surface,
+         call this method with same mime type and :obj:`None` for data.
+
+        The attached image (or filename) data can later
+        be used by backends which support it
+        (currently: PDF, PS, SVG and Win32 Printing surfaces)
+        to emit this data instead of making a snapshot of the surface.
+        This approach tends to be faster
+        and requires less memory and disk space.
+
+        The recognized MIME types are the following:
+
+        ``"image/png"``
+            The Portable Network Graphics image file format (ISO/IEC 15948).
+        ``"image/jpeg"``
+            The Joint Photographic Experts Group (JPEG)
+            image coding standard (ISO/IEC 10918-1).
+        ``"image/jp2"``
+            The Joint Photographic Experts Group (JPEG) 2000
+            image coding standard (ISO/IEC 15444-1).
+        ``"text/x-uri"``
+            URL for an image file (unofficial MIME type).
+
+        See corresponding backend surface docs
+        for details about which MIME types it can handle.
+        Caution: the associated MIME data will be discarded
+        if you draw on the surface afterwards.
+        Use this function with care.
+
+        :param mime_type: The MIME type of the image data.
+        :type mime_type: ASCII string
+        :param data: The image data to attach to the surface.
+        :type data: bytes
+
+        New in cairo 1.10.
+
+        """
         mime_type = ffi.new('char[]', mime_type.encode('utf8'))
         if data is None:
             _check_status(cairo.cairo_surface_set_mime_data(
@@ -267,22 +375,97 @@ class Surface(object):
                 *keep_alive.closure))
             keep_alive.save()  # Only on success
 
+    def get_mime_data(self, mime_type):
+        """Return mime data previously attached to surface
+        using the specified mime type.
+
+        :param mime_type: The MIME type of the image data.
+        :type mime_type: ASCII string
+        :returns:
+            A CFFI buffer object, or :obj:`None`
+            if no data has been attached with the given mime type.
+
+        New in cairo 1.10.
+
+        """
+        buffer_address = ffi.new('unsigned char **')
+        buffer_length = ffi.new('unsigned long *')
+        mime_type = ffi.new('char[]', mime_type.encode('utf8'))
+        cairo.cairo_surface_get_mime_data(
+            self._pointer, mime_type, buffer_address, buffer_length)
+        return (ffi.buffer(buffer_address[0], buffer_length[0])
+                if buffer_address[0] != ffi.NULL else None)
+
     def supports_mime_type(self, mime_type):
+        """ Return whether surface supports :obj:`mime_type`.
+
+        :param mime_type: The MIME type of the image data.
+        :type mime_type: ASCII string
+
+        New in cairo 1.12.
+
+        """
         mime_type = ffi.new('char[]', mime_type.encode('utf8'))
         return bool(cairo.cairo_surface_supports_mime_type(
             self._pointer, mime_type))
 
     def mark_dirty(self):
+        """Tells cairo that drawing has been done to surface
+        using means other than cairo,
+        and that cairo should reread any cached areas.
+        Note that you must call :meth:`flush` before doing such drawing.
+
+        """
         cairo.cairo_surface_mark_dirty(self._pointer)
         self._check_status()
 
     def mark_dirty_rectangle(self, x, y, width, height):
+        """
+        Like :meth:`mark_dirty`,
+        but drawing has been done only to the specified rectangle,
+        so that cairo can retain cached contents
+        for other parts of the surface.
+
+        Any cached clip set on the surface will be reset by this function,
+        to make sure that future cairo calls have the clip set
+        that they expect.
+
+        :param x: X coordinate of dirty rectangle.
+        :type x: float
+        :param y: Y coordinate of dirty rectangle.
+        :type y: float
+        :param width: Width of dirty rectangle.
+        :type width: float
+        :param height: Height of dirty rectangle.
+        :type height: float
+
+        """
         cairo.cairo_surface_mark_dirty_rectangle(
             self._pointer, x, y, width, height)
         self._check_status()
 
     def show_page(self):
+        """Emits and clears the current page
+        for backends that support multiple pages.
+        Use :meth:`copy_page` if you don't want to clear the page.
+
+        :meth:`Context.show_page` is a convenience method for this.
+
+        """
         cairo.cairo_surface_show_page(self._pointer)
+        self._check_status()
+
+    def copy_page(self):
+        """Emits the current page for backends that support multiple pages,
+        but doesn't clear it,
+        so that the contents of the current page will be retained
+        for the next page.
+
+        Use :meth:`show_page` if you want to get an empty page
+        after the emission.
+
+        """
+        cairo.cairo_surface_copy_page(self._pointer)
         self._check_status()
 
     def flush(self):
