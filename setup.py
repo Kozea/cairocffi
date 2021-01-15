55cd4fc49d55dce6a48363b3daf7a1a6e9fe2b34
Armand Foucault <armand.foucault@telecom-bretagne.eu>
2021-01-15 11:49:07 +0100

    Read version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import os
 import sys
 
 from setuptools import setup
+
+
+VERSION_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "cairocffi", "VERSION")
+with open(VERSION_FILE, "r") as fd:
+    version = fd.read().strip()
+
 
 if sys.version_info.major < 3:
     raise RuntimeError(
@@ -8,6 +15,7 @@ if sys.version_info.major < 3:
         'Please use Python 3 or install an older version of cairocffi.')
 
 setup(
+    version=version,
     cffi_modules=[
         'cairocffi/ffi_build.py:ffi',
         'cairocffi/ffi_build.py:ffi_pixbuf']
