0dcde9d43838af00de8e178392a9f3819c921998
Lucie Anglade <lucie@courtbouillon.org>
2021-10-01 17:56:55 +0200

    Fix isort…

--- cairocffi/ffi_build.py
+++ cairocffi/ffi_build.py
@@ -9,7 +9,7 @@
 
 """
 
-from importlib.util import spec_from_file_location, module_from_spec
+from importlib.util import module_from_spec, spec_from_file_location
 from pathlib import Path
 
 from cffi import FFI
