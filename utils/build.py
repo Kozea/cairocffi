bd07dc65884af88e11da61f8f47015287903087b
Guillaume Ayoub <guillaume@courtbouillon.org>
2023-03-17 16:32:03 +0100

    Include all Setuptool’s building functions in custom building script

--- utils/build.py
+++ utils/build.py
@@ -2,11 +2,8 @@ import importlib.util
 import sys
 from pathlib import Path
 
-from setuptools import build_meta as _orig
-
-prepare_metadata_for_build_wheel = _orig.prepare_metadata_for_build_wheel
-get_requires_for_build_wheel = _orig.get_requires_for_build_wheel
-get_requires_for_build_sdist = _orig.get_requires_for_build_sdist
+from setuptools import build_meta
+from setuptools.build_meta import *  # noqa
 
 folder = Path(__file__).parent.parent / 'cairocffi'
 
@@ -14,7 +11,7 @@ folder = Path(__file__).parent.parent / 'cairocffi'
 def build_sdist(*args, **kwargs):
     (folder / '_generated' / 'ffi.py').unlink(missing_ok=True)
     (folder / '_generated' / 'ffi_pixbuf.py').unlink(missing_ok=True)
-    return _orig.build_sdist(*args, **kwargs)
+    return build_meta.build_sdist(*args, **kwargs)
 
 
 def build_wheel(*args, **kwargs):
@@ -24,4 +21,4 @@ def build_wheel(*args, **kwargs):
     sys.modules['ffi_build'] = module
     spec.loader.exec_module(module)
     module.compile()
-    return _orig.build_wheel(*args, **kwargs)
+    return build_meta.build_wheel(*args, **kwargs)
