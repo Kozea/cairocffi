508f0c311384fdc5fb5e7c8e170965c9f792efd1
Guillaume Ayoub <guillaume@courtbouillon.org>
2023-06-12 10:42:46 +0200

    Update documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,15 @@
-from pathlib import Path
+import cairocffi
 
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage']
 master_doc = 'index'
 project = 'cairocffi'
-copyright = '2013-2019, Simon Sapin'
-release = (
-    Path(__file__).parent.parent / 'cairocffi' / 'VERSION').read_text().strip()
-version = '.'.join(release.split('.')[:2])
+copyright = 'Simon Sapin and contributors'
+release = cairocffi.version
+version = release
 exclude_patterns = ['_build']
 autodoc_member_order = 'bysource'
 autodoc_default_flags = ['members']
 intersphinx_mapping = {
-    'http://docs.python.org/': None,
+    'http://docs.python.org/3/': None,
     'http://cairographics.org/documentation/pycairo/2/': None}
