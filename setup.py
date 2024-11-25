0fdeadce344ef5c9e7290e184178b9ebdde2d8fa
George Harker <george@george-graphics.co.uk>
2024-11-25 11:38:29 -0800

    Fix non api mode

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ from distutils.command.build import build
 
 api_mode = False
 if ('CAIROCFFI_API_MODE' in os.environ and
-        int(os.environ['CAIROCFFI_API_MODE']) == 1):
+        int(os.environ['CAIROCFFI_API_MODE']) == 2):
     api_mode = True
 
 setup(
     name='cairocffi',
-    #use_scm_version=True,
+    use_scm_version=True,
     version='1.7.2',
     install_requires=['cffi >= 1.1.0', 'xcffib >= 1.5.1'],
     setup_requires=['setuptools_scm', 'cffi >= 1.1.0', 'xcffib >= 1.5.1'],
-    packages=['cairocffi'] if api_mode else [],
-    cffi_modules=['cairocffi/ffi_build.py:ffi']
+    packages=['cairocffi'],
+    cffi_modules=['cairocffi/ffi_build.py:ffi'] if api_mode else []
 )
